747333c176d3b693d627ec87c38969aa8f697f41
Ignacio Fantini <ifantini@gmail.com>
2022-01-09 16:18:53 -0300

    feat(add telegram-send): HallacaBot send messages

--- boil_alert.py
+++ boil_alert.py
@@ -1,5 +1,5 @@
 from temp_reader import read_temp
-import time
+import time, telegram_send
 
 temp = read_temp()
 start_time = time.time()
@@ -10,11 +10,13 @@ while temp < 95:
     time.sleep(10)
 
 boil_time = time.time()
-print("Hirvió en {}s".format(boil_time - start_time))
+message = "Hirvió en {:.0f}s".format(boil_time - start_time)
+telegram_send.send(messages=[message])
 
 while time.time() - boil_time < 15 * 60:
     temp = read_temp()
     print("La tempratura está en {:.1f}ºC y llevamos {:.0f}s de 900s desde que hirvió".format(temp, time.time() - start_time))
     time.sleep(10)
 
-print("Están listas las hallacas! nos demoramos {:.0f}s en todo el proceso".format(time.time() - start_time))
+message = "Están listas las hallacas! nos demoramos {:.0f}s en todo el proceso".format(time.time() - start_time)
+telegram_send.send(messages=[message])
